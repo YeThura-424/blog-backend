0fc4ce68c9bd4dfb6fefce917a7494eb51a3189f
YeThura-424 <rathanpoudle.26@gmail.com>
2024-04-28 22:32:18 +0630

    nodemon, route

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+// import express from "express";
 const express = require("express");
 const app = express();
-const PORT = 500;
+const PORT = 5000;
+
+//routes
+
+app.get("/", (req, res) => {
+  res.send("Hello World Server");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Hello Home Page");
+});
+
+app.get("/category", (req, res) => {
+  res.send("Hello Category Page");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
