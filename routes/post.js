e8d7ce53e4e12a8a816fc0f8eebca5d68e129e42
YeThura-424 <rathanpoudle.26@gmail.com>
2024-05-07 22:44:05 +0630

    insert post to mongodb

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,36 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const Post = mongoose.model("Post");
 
-router.get("/posts", (req, res) => {
-  //post router
+router.get("/post", (req, res) => {
+  Post.find()
+    .populate("category", "_id name")
+    .then((posts) => {
+      res.json({ posts });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/new-post", (req, res) => {
+  const { title, description, imgUrl } = req.body;
+  //validation for fields
+  if (!title || !description || !imgUrl) {
+    res.json({ message: "All fields are required !!" });
+  }
+
+  const post = new Post({
+    title,
+    description,
+    imgUrl,
+  });
+  post
+    .save()
+    .then(() => {
+      res.json({ message: "Post Created Successfully" });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 module.exports = router;
